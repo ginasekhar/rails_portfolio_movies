144fb62e9bfea26863cf00d8ad5d47581392aed1
Gina Sekhar <ginasekhar@yahoo.com>
2020-01-12 22:15:54 -0500

    Add oauth functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   resources :movies
-  devise_for :users, :controllers => {registrations: 'registrations' }
+  devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks' }
+
+  devise_scope :user do 
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do 
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'application#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_040429) do
+ActiveRecord::Schema.define(version: 2020_01_12_182943) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_01_12_040429) do
     t.string "name"
     t.integer "age"
     t.boolean "theater_owner"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
